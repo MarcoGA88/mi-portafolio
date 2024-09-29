020d483dbb9162945e9e01636e331e4c2801eb05
MarcoGA88 <antonigabilanes102@gmail.com>
2024-09-29 12:36:41 -0500

    Social1

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -28,14 +28,14 @@ function Contact() {
                 required
                 className='p-4 rounded-lg bg-gray-700 text-base w-full focus:border-emerald-300 focus:outline-none shadow-sm border border-black focus:right-1 focus:ring-emerald-300'
                 placeholder='Nombre'
-              ></input>
+              />
               <input
                 type='email'
                 name='email'
                 required
                 className='p-4 rounded-lg bg-gray-700 text-base w-full focus:border-emerald-300 focus:outline-none shadow-sm border border-black focus:right-1 focus:ring-emerald-300'
                 placeholder='Email'
-              ></input>
+              />
             </div>
 
             <div className='grid grid-cols-1 gap-6'>
@@ -45,11 +45,11 @@ function Contact() {
                 rows={6}
                 className='w-full rounded-lg bg-gray-700 focus:border-emerald-300 p-4 shadow-sm border border-black focus:outline-none focus:right-1 focus:ring-emerald-300 text-base'
                 placeholder='Escribe un mensaje...'
-              ></textarea>
+              />
             </div>
-            <AnimatedButton></AnimatedButton>
-            <input type='hidden' name='_next' value={"https://immarcogm.vercel.app/contact"}></input>
-            <input type='hidden' name='_captcha' value={"false"}></input>
+            <AnimatedButton />
+            <input type='hidden' name='_next' value={"https://immarcogm.vercel.app/contact"} />
+            <input type='hidden' name='_captcha' value={"false"} />
           </form>
         </div>
 
@@ -58,7 +58,9 @@ function Contact() {
           <h2 className='text-xl sm:text-2xl md:text-3xl mb-6 pt-8'>Contacto</h2>
           <div className="flex items-center space-x-3 text-gray-500 pt-4 text-lg sm:text-base">
             <Mail color='rgb(110 231 183)' />
-            <span>antonigabilanes102@gmail.com</span>
+            <a href="mailto:antonigabilanes102@gmail.com" className="hover:underline">
+              antonigabilanes102@gmail.com
+            </a>
           </div>
           <div className="flex items-center space-x-3 text-gray-500 pt-4 text-lg sm:text-base">
             <Phone color='rgb(110 231 183)' />
@@ -67,22 +69,24 @@ function Contact() {
 
           {/* Social Media Links */}
           <h2 className='text-xl sm:text-2xl md:text-3xl mb-6 pt-8'>Social</h2>
-          <div className="flex items-center space-x-3 text-gray-500 pt-4 text-lg sm:text-base">
-            <Instagram color='rgb(110 231 183)' />
-            <span>Instagram</span>
-          </div>
-          <div className="flex items-center space-x-3 text-gray-500 pt-4 text-lg sm:text-base">
-            <Linkedin color='rgb(110 231 183)' />
-            <span>Linkedin</span>
-          </div>
-          <div className="flex items-center space-x-3 text-gray-500 pt-4 text-lg sm:text-base">
-            <Github color='rgb(110 231 183)' />
-            <span>Github</span>
+          <div className="flex flex-col space-y-3">
+            <div className="flex items-center space-x-3 text-gray-500 pt-4 text-lg sm:text-base">
+              <Linkedin color='rgb(110 231 183)' />
+              <a href="https://www.linkedin.com/in/marco-antonio-gabilanes-mart%C3%ADnez-06386619a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer" className="hover:underline">
+                LinkedIn
+              </a>
+            </div>
+            <div className="flex items-center space-x-3 text-gray-500 pt-4 text-lg sm:text-base">
+              <Github color='rgb(110 231 183)' />
+              <a href="https://github.com/MarcoGA88" target="_blank" rel="noopener noreferrer" className="hover:underline">
+                GitHub
+              </a>
+            </div>
           </div>
         </div>
       </div>
 
-      <Footer></Footer>
+      <Footer />
     </div>
   );
 }
